2229c36bb01472b8785d1188d96e34e0ac46bbcf
thiberaw <thibautware@outlook.com>
2020-11-19 15:53:05 +0000

    Handle editor init and events

--- addon/components/tinymce-ember.js
+++ addon/components/tinymce-ember.js
@@ -6,32 +6,87 @@ import { scheduleOnce } from '@ember/runloop';
 export default Component.extend({
   layout,
 
+  editor: null,
+  editorContent: '',
+  editorId: null,
   editorName: 'tinymce',
 
   init() {
     this._super(...arguments);
 
-    // Generate editor's id for textarea
-    this.set('editorId', `editor-${guidFor(this)}`);
+    // Generate and set editor's id for textarea
+    const EditorId = `${this.editorName}-${guidFor(this)}`
+      .replaceAll(/[^a-z0-9]/gi, '-')
+      .toLowerCase();
+    this.set('editorId', EditorId);
+
+    // Run editor initialization once page is rendered
+    scheduleOnce('afterRender', this, this.initEditor);
   },
 
-  didReceiveAttrs() {
+  willDestroyElement() {
     this._super(...arguments);
 
-    // Initialize only if no active editor
-    if (!window.tinymce.activeEditor) {
-      // Run editor initialisation once page is rendered
-      scheduleOnce('afterRender', this, this.initEditor);
+    const Editor = this.editor;
+    if (Editor) {
+      Editor.off('change keyup setcontent', this.handleEditorChange.bind(this));
+
+      if (this.customEvents) {
+        this.customEvents.forEach(event => {
+          Editor.off(event.name, event.handler);
+        });
+      }
+
+      Editor.remove();
+    }
+  },
+
+  handleEditorChange() {
+    const Editor = this.editor;
+    if (Editor) {
+      const NewContent = Editor.getContent({format: 'html'});
+
+      if (this.editorContent !== NewContent) {
+        this.set('editorContent', NewContent);
+      }
+    }
+  },
+
+  handleEditorInit() {
+    const Editor = this.editor;
+    if (Editor) {
+      Editor.setContent(this.editorContent);
+
+      Editor.on('change keyup setcontent', this.handleEditorChange.bind(this));
+
+      if (this.customEvents) {
+        this.customEvents.forEach(event => {
+          Editor.on(event.name, event.handler);
+        });
+      }
     }
   },
 
   initEditor() {
+    // Importing tinymce module in your web page will run an IIFE that attaches tinymce to the window
     if (!window.tinymce) {
       throw new Error('TinyMCE hasn\'t been attached to the window hence not imported in your application.');
     }
 
-    this.config.selector = `#${this.editorId}`;
+    // Extend default and custom configurations
+    const Config = {
+      ...this.config,
+      selector: `#${this.editorId}`,
+      setup: editor => {
+        this.set('editor', editor);
+        editor.on('init', this.handleEditorInit.bind(this));
 
-    window.tinymce.init(this.config);
+        if (this.config.setup && typeof this.config.setup === 'function') {
+          this.config.setup(editor);
+        }
+      }
+    }
+
+    window.tinymce.init(Config);
   }
 });
