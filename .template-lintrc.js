ef7851226149abee2e6a6823fb8d19b70f368751
thiberaw <thibautware@outlook.com>
2020-11-25 17:00:27 +0000

    Unset octane on template linter

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane'
+  extends: ['recommended'],
+  rules: {
+    // Recommended rules we override
+    'no-inline-styles': false,
+    'simple-unless': {
+      maxHelpers: -1
+    },
+    'table-groups': false,
+
+    // Added rules to preset
+    'deprecated-each-syntax': true,
+    'deprecated-inline-view-helper': true,
+    'eol-last': 'never',
+    'inline-link-to': true,
+    'linebreak-style': true,
+    'no-trailing-spaces': true,
+    'no-unnecessary-concat': true,
+    'self-closing-void-elements': true,
+    'template-length': {
+      max: 825,
+      min: 1
+    }
+  }
 };
