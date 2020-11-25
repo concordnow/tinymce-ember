0d5c6aa9b070968563e3bc509af376665a1aa7d6
thiberaw <thibautware@outlook.com>
2020-11-25 10:30:51 +0000

    Test basic editor configuration param

--- tests/integration/components/tinymce-ember-test.js
+++ tests/integration/components/tinymce-ember-test.js
@@ -6,20 +6,40 @@ import { hbs } from 'ember-cli-htmlbars';
  /* eslint-disable no-unused-vars */
 import tinymce from 'tinymce/tinymce';
 
+const CONFIG = {
+  base_url: 'tinymce',
+  theme: 'silver',
+  toolbar:
+  'undo redo | fontselect | fontsizeselect | forecolor backcolor | align lineheight | bullist numlist'
+};
+
 module('Integration | Component | tinymce-ember', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  hooks.beforeEach(function() {
+    this.setProperties({
+      config: CONFIG,
+      editorContent: ''
+    });
+  });
+
+  test('it renders on its own', async function(assert) {
     await render(hbs`<TinymceEmber />`);
 
     assert.equal(this.element.textContent.trim(), '');
   });
 
-  test('it renders with content', async function(assert) {
-    this.set('editorContent', '');
+  test('it renders with config', async function(assert) {
+    await render(hbs`<TinymceEmber @config={{this.config}} />`);
 
+    assert.equal(tinymce.activeEditor.getParam('base_url'), CONFIG.base_url);
+    assert.equal(tinymce.activeEditor.getParam('theme'), CONFIG.theme);
+    assert.equal(tinymce.activeEditor.getParam('toolbar'), CONFIG.toolbar);
+  });
+
+  test('it renders with config & content', async function(assert) {
     await render(hbs`
-      <TinymceEmber @editorContent={{this.editorContent}}/>
+      <TinymceEmber @config={{this.config}} @editorContent={{this.editorContent}}/>
     `);
 
     this.set('editorContent', '<p>template block text</p>');
