74c761b7d78b6da5069a57e17ea6b19ac6fde926
thiberaw <thibautware@outlook.com>
2020-11-30 10:25:23 +0000

    Remove line feed in test file

--- tests/integration/components/tinymce-ember-test.js
+++ tests/integration/components/tinymce-ember-test.js
@@ -24,7 +24,7 @@ module('Integration | Component | tinymce-ember', function(hooks) {
   });
 
   test('it renders on its own', async function(assert) {
-    await render(hbs`<TinymceEmber />`);
+    await render(hbs`<TinymceEmber/>`);
 
     assert.equal(this.element.textContent.trim(), '');
   });
@@ -38,7 +38,7 @@ module('Integration | Component | tinymce-ember', function(hooks) {
   });
 
   test('it renders with config', async function(assert) {
-    await render(hbs`<TinymceEmber @config={{this.config}} />`);
+    await render(hbs`<TinymceEmber @config={{this.config}}/>`);
 
     assert.equal(tinymce.activeEditor.getParam('base_url'), CONFIG.base_url);
     assert.equal(tinymce.activeEditor.getParam('theme'), CONFIG.theme);
@@ -60,9 +60,7 @@ module('Integration | Component | tinymce-ember', function(hooks) {
       <TinymceEmber
         @config={{this.config}}
         @editorContent={{this.editorContent}}
-        @onEditorContentChange={{action this.contentUpdateAction}}
-      />
-    `);
+        @onEditorContentChange={{action this.contentUpdateAction}}/>`);
 
     await waitUntil(() => {
       return tinymce.activeEditor.getContent({format: 'html'}) === checkValue;
@@ -87,8 +85,7 @@ module('Integration | Component | tinymce-ember', function(hooks) {
       <TinymceEmber
         @config={{this.config}}
         @editorContent={{this.editorContent}}
-        @onEditorContentChange={{action this.contentUpdateAction}}
-      />`);
+        @onEditorContentChange={{action this.contentUpdateAction}}/>`);
 
     await waitUntil(() => {
       return tinymce.activeEditor.getContent({format: 'html'}) === value;
@@ -115,8 +112,7 @@ module('Integration | Component | tinymce-ember', function(hooks) {
 
     await render(hbs`<TinymceEmber
       @editorContent={{this.editorContent}}
-      @onEditorContentChange={{action this.contentUpdateAction}}/>
-    `);
+      @onEditorContentChange={{action this.contentUpdateAction}}/>`);
 
     await waitUntil(() => {
       return tinymce.activeEditor.getContent({format: 'html'}) === value;
