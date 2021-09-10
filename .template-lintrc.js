8a8691a36ae315dcac93bec16a72197b372ae7f2
Camille TJHOA <camille.tjhoa@concordnow.com>
2021-09-10 16:05:15 +0000

    Update template-lint config

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,27 +1,6 @@
 'use strict';
 
 module.exports = {
-  extends: ['recommended'],
-  rules: {
-    // Recommended rules we override
-    'no-inline-styles': false,
-    'simple-unless': {
-      maxHelpers: -1
-    },
-    'table-groups': false,
-
-    // Added rules to preset
-    'deprecated-each-syntax': true,
-    'deprecated-inline-view-helper': true,
-    'eol-last': 'never',
-    'inline-link-to': true,
-    'linebreak-style': true,
-    'no-trailing-spaces': true,
-    'no-unnecessary-concat': true,
-    'self-closing-void-elements': true,
-    'template-length': {
-      max: 825,
-      min: 1
-    }
-  }
+  plugins: ['ember-template-lint-plugin-prettier'],
+  extends: ['octane', 'ember-template-lint-plugin-prettier:recommended'],
 };
