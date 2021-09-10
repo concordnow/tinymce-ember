8ac00d34073159b5bd55b5fae1fb4aa8542d183c
Camille TJHOA <camille.tjhoa@concordnow.com>
2021-09-10 15:31:19 +0000

    Move to ember-concord-doc

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,17 +1,20 @@
-import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import EmberRouter from '@ember/routing/router';
 import config from 'dummy/config/environment';
 
-export default class Router extends AddonDocsRouter {
+export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
 Router.map(function() {
-  docsRoute(this, function() {
+  this.route('docs', function () {
     this.route('simple');
     this.route('inline');
     this.route('target');
     this.route('disable');
+    this.route('modifiers', function () {
+      this.route('editor');
+    });
   });
   this.route('not-found', { path: '/*path' });
 });
